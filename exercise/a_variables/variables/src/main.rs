85de38df8070265e9461feceda7b7f4fec3a924e
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-03-19 20:14:20 +0545

    exercise three completed

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,10 @@
+const STARTING_MISSILES : i32 = 8;
+const READY_AMOUNT : i32 = 2;
+fn main() {
+    
+    let unused_variable = 3;
+    let ( missiles,ready) : (i32,i32) = (STARTING_MISSILES,READY_AMOUNT);
+    // let ready : i32 = READY_AMOUNT;
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles - ready);
+}
